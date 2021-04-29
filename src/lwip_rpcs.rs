6513e558526bcd74e19a35fb17e585e0b5128c8a
nmiyamoto <scotsdowneroad@gmail.com>
2021-04-29 14:14:05 +0900

    impl Select

--- src/lwip_rpcs.rs
+++ src/lwip_rpcs.rs
@@ -195,3 +195,116 @@ impl super::RPC for Connect{
         Ok(num)
     }
 }
+
+
+/// lwip Connect function 
+//int lwip_select(int maxfdp1, fd_set *readset, fd_set *writeset, fd_set *exceptset, struct timeval *timeout)
+pub struct Select{
+    pub s: i32,
+    pub readset:   Option<super::FdSet>,
+    pub writeset:  Option<super::FdSet>,
+    pub exceptset: Option<super::FdSet>,
+    pub timeval:   Option<super::TimeVal>,
+}
+
+impl super::RPC for Select{
+    type ReturnValue = i32;
+    type Error = ();
+
+    fn args(&self, buff: &mut heapless::Vec<u8, heapless::consts::U64>) {
+        let s = self.s as i32;
+        let readset = &self.readset;
+        let writeset = &self.writeset;
+        let exceptset = &self.exceptset;
+        let timeval = &self.timeval;
+        const FDSET_LEN:u32= core::mem::size_of::<super::FdSet>() as u32;
+        const TIME_LEN:u32= core::mem::size_of::<super::TimeVal>() as u32;
+        
+        //TODO
+        // write file discripter
+        buff.extend_from_slice(&s.to_le_bytes()).ok();
+
+        //write readset null flug u8
+        //write readset binary (size : uint32 and data u8xsize)
+        match readset {
+            None => {
+                let null_flag = 1u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+            },
+            Some(i) => {
+                let null_flag = 0u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+                buff.extend_from_slice(&FDSET_LEN.to_le_bytes()).ok();
+                buff.extend_from_slice(&i.fd_bits).ok();
+            },
+        }
+
+        //write writeset null flug u8
+        //write writeset binary (size : uint32 and data u8xsize
+        match writeset{
+            None => {
+                let null_flag = 1u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+            },
+            Some(i) => {
+                let null_flag = 0u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+                buff.extend_from_slice(&FDSET_LEN.to_le_bytes()).ok();
+                buff.extend_from_slice(&i.fd_bits).ok();
+            },
+        }
+
+        //write exceptset null flug u8
+        //write exceptset binary (size : uint32 and data u8xsize
+        match exceptset{
+            None => {
+                let null_flag = 1u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+            },
+            Some(i) => {
+                let null_flag = 0u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+                buff.extend_from_slice(&FDSET_LEN.to_le_bytes()).ok();
+                buff.extend_from_slice(&i.fd_bits).ok();
+            },
+        }
+
+        //write timeout null flug u8
+        //write timeout binary (size : uint32 and data u8xsize
+        match timeval{
+            None => {
+                let null_flag = 1u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+            },
+            Some(i) => {
+                let null_flag = 0u8;
+                buff.extend_from_slice(&null_flag.to_le_bytes()).ok();
+                buff.extend_from_slice(&TIME_LEN.to_le_bytes()).ok();
+                buff.extend_from_slice(&i.tv_sec.to_le_bytes()).ok();
+                buff.extend_from_slice(&i.tv_usec.to_le_bytes()).ok();
+            },
+        }
+    }
+
+    fn header(&self, seq: u32) -> codec::Header {
+        codec::Header {
+            sequence: seq,
+            msg_type: ids::MsgType::Invocation,
+            service: ids::Service::LWIP,
+            request: ids::LWIPRequest::Select.into(),
+        }
+    }
+
+    fn parse(&mut self, data: &[u8]) -> Result<Self::ReturnValue, Err<Self::Error>> {
+        let (data, hdr) = codec::Header::parse(data)?;
+        if hdr.msg_type != ids::MsgType::Reply
+            || hdr.service != ids::Service::LWIP
+            || hdr.request != ids::LWIPRequest::Select.into()
+        {
+            return Err(Err::NotOurs);
+        }
+
+        let (_, num) = streaming::le_i32(data)?;
+        Ok(num)
+    }
+}
